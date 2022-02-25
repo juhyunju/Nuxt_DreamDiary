474de28db87d3848fe8750170a95837dadcd8c22
juhyunju <2juhyunju@naver.com>
2022-02-25 17:44:27 +0900

    로그인,회원가입,마이페이지,메인 라우터 설정 까지

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark>
+      <Nuxt />
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title><NuxtLink to="/" style="text-decoration: none; color: #fff;"> DreamDiary</NuxtLink></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn><NuxtLink to="/login" style="text-decoration: none; color: #fff;">로그인</NuxtLink></v-btn>
+      <v-btn><NuxtLink to="/sign" style="text-decoration: none; color: #fff;">회원가입</NuxtLink></v-btn>
+      <v-btn><NuxtLink to="/myhome" style="text-decoration: none; color: #fff;">마이페이지</NuxtLink></v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<style lang="css">
+</style>
